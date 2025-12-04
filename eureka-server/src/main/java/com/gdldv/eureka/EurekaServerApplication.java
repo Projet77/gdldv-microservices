e6a172a6e7630fbb9658bf974ade9b5fde27b0b0
lakhat1 <Abdoulahatbah@gmail.com>
2025-12-04 12:18:19 +0000

    feat(GDLDV-567): add Eureka Server for service discovery

---
+++ eureka-server/src/main/java/com/gdldv/eureka/EurekaServerApplication.java
@@ -0,0 +1,22 @@
+package com.gdldv.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka Server Application
+ * Service de découverte pour l'architecture microservices GDLDV
+ *
+ * @author GDLDV Team
+ * @version 1.0.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
